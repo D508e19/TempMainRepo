e6d71aaa292db4fa84f56fd375c84db38602a49d
judo347 <mikkelhvk@hotmail.com>
2019-10-17 12:40:19 +0200

    added loopfunction that collects all alibots

---
+++ loop_functions/communicator_loop_functions/communicator_loop_functions.cpp
@@ -0,0 +1,76 @@
+#include "communicator_loop_functions.h"
+
+#include <argos3/plugins/robots/foot-bot/simulator/footbot_entity.h>
+#include <argos3/core/utility/logging/argos_log.h>
+#include <argos3/plugins/robots/foot-bot/simulator/footbot_entity.h>
+#include <controllers/alibot/alibot.h>
+#include <argos3/core/utility/math/vector2.h>
+
+#include <list>
+#include <iterator>
+
+/****************************************/
+
+CommunicatorLoopFunctions::CommunicatorLoopFunctions() {
+}
+
+CommunicatorLoopFunctions::~CommunicatorLoopFunctions(){
+
+}
+
+/****************************************/
+/****************************************/
+
+void CommunicatorLoopFunctions::Init(TConfigurationNode& t_tree){
+   
+   CollectBotControllers();
+}
+
+void CommunicatorLoopFunctions::PreStep(){
+
+}
+
+/* Delegates destinations to controllers that is available. /
+void CommunicatorLoopFunctions::DelegateDestinations(){
+
+   /* Iterate through all controllers and check if bot is ready for directions /
+   std::list<Alibot*>::iterator it = this->botControllers.begin(); 
+   std::list<Alibot*>::iterator end = this->botControllers.end();
+    
+   for(it; it != end; it++){
+
+      /* Is this bot ready for directions? /
+      if((*it)->IsReadyForDestination()){
+
+         /* Is there any directions left to give? /
+         if(destinations.size() != 0){
+            std::list<CVector2>::iterator itDest = destinations.begin();
+
+            CVector2 dest = *itDest;
+            destinations.pop_front();
+            (*it)->SetDestination(dest);
+         }
+      }
+   }
+}/*
+
+/* Find and collects all foot-bot controllers.
+ * (Only works with alibots) */
+void CommunicatorLoopFunctions::CollectBotControllers(){
+   
+   /* Get a map containing all active foot-bots */
+   CSpace::TMapPerType& botMap = GetSpace().GetEntitiesByType("foot-bot");
+
+   /* Iterate through all bots and collect their controller */
+   for(CSpace::TMapPerType::iterator it = botMap.begin(); it != botMap.end(); it++){
+      CFootBotEntity& cFootbot = *any_cast<CFootBotEntity*>(it->second); //Get the footbot
+      Alibot& controller = dynamic_cast<Alibot&>(cFootbot.GetControllableEntity().GetController()); //Get the controller of the footbot
+      Alibot *pr = &controller;
+      botControllers.push_back(pr); //Put it a the end of the list
+   }
+}
+
+/****************************************/
+/****************************************/
+
+REGISTER_LOOP_FUNCTIONS(CommunicatorLoopFunctions, "communicator_loop_functions");
---
+++ loop_functions/communicator_loop_functions/communicator_loop_functions.h
@@ -0,0 +1,35 @@
+#ifndef COMMUNICATOR_LOOP_FUNCTIONS_H
+#define COMMUNICATOR_LOOP_FUNCTIONS_H
+
+#include <argos3/core/simulator/loop_functions.h>
+#include <argos3/plugins/robots/foot-bot/simulator/footbot_entity.h>
+#include <controllers/alibot/alibot.h>
+#include <argos3/core/control_interface/ci_controller.h>
+#include <argos3/core/utility/math/vector2.h>
+
+#include <list>
+
+using namespace argos;
+
+class CommunicatorLoopFunctions : public CLoopFunctions {
+
+public:
+
+   CommunicatorLoopFunctions();
+   virtual ~CommunicatorLoopFunctions();
+
+   virtual void Init(TConfigurationNode& t_tree);
+
+   virtual void PreStep();
+
+private:
+
+   /* A list for all controllers */
+   std::list<Alibot*> botControllers;
+
+   /* Find and collects all foot-bot controllers.
+    * (Only works with alibots) */
+   void CollectBotControllers();
+};
+
+#endif
