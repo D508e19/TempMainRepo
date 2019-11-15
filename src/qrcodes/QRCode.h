4d64d3259244decc78bc80dd96051213d1b494ae
judo347 <mikkelhvk@hotmail.com>
2019-11-15 12:02:18 +0100

    Added position to the QRCode class and used it in Basicbot.

--- src/qrcodes/QRCode.h
+++ src/qrcodes/QRCode.h
@@ -1,11 +1,19 @@
 #ifndef QRCODES_H
 #define QRCODES_H
 
+#include <argos3/core/utility/math/vector2.h>
+
 class QRCode
 {
 private:
+
+    argos::CVector2 position;
     
 public:
+
+    argos::CVector2 GetPosition();
+    void SetPosition(argos::CVector2 newPosition);
+    
     QRCode();
     ~QRCode();
 };
@@ -14,5 +22,12 @@ QRCode::QRCode(){}
 
 QRCode::~QRCode(){}
 
+argos::CVector2 QRCode::GetPosition(){
+    return position;
+}
+
+void QRCode::SetPosition(argos::CVector2 newPosition){
+    position = newPosition;
+}
 
 #endif
