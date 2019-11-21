cde81b3cbc2a43552d4f726c735545d6fbb0edaa
chriskrejler <christopher@krejler.dk>
2019-11-21 12:22:53 +0100

    Path in (hopefully) right order
    test

--- src/pathfinder/Pathfinder.h
+++ src/pathfinder/Pathfinder.h
@@ -1,6 +1,7 @@
 #ifndef PATHFINDER_H
 #define PATHFINDER_h
 
+#include <environment/pathfinding/aStar.h>
 #include "src/datatypes/Coordinate.h"
 #include "src/datatypes/Path.h"
 
@@ -13,27 +14,30 @@ public:
     Pathfinder();
     Pathfinder(Coordinate);
     ~Pathfinder();
-   
+    aStar astar;
+
     Path GetStupidPath(Coordinate start, Coordinate end);
 };
 
 Pathfinder::Pathfinder(){}
 Pathfinder::~Pathfinder(){}
 
+aStar::aStar(){}
+
 Path Pathfinder::GetStupidPath(Coordinate start, Coordinate end)
 {
-    //argos::LOG << "Find path to: "; 
-    //end.PrintCoord();
-    std::cout << "Want to go to: " << end.x << ", " << end.y << std::endl;
-    Path newPath;
-    //std::cout << "Adding waypoint:" << start.x << " " << start.y << std::endl;
-    //newPath.AddWayPoint(start);
-    std::cout << "Adding waypoint:" << end.x << " " << start.y << std::endl;
-    newPath.AddWayPoint(Coordinate(end.x, start.y), start);
-    std::cout << "Adding waypoint:" << end.x << " " << end.y << std::endl;
-    newPath.AddWayPoint(Coordinate(end.x, end.y), Coordinate(end.x, start.y));
+//    //argos::LOG << "Find path to: ";
+//    //end.PrintCoord();
+//    std::cout << "Want to go to: " << end.x << ", " << end.y << std::endl;
+//    Path newPath;
+//    //std::cout << "Adding waypoint:" << start.x << " " << start.y << std::endl;
+//    //newPath.AddWayPoint(start);
+//    std::cout << "Adding waypoint:" << end.x << " " << start.y << std::endl;
+//    newPath.AddWayPoint(Coordinate(end.x, start.y), start);
+//    std::cout << "Adding waypoint:" << end.x << " " << end.y << std::endl;
+//    newPath.AddWayPoint(Coordinate(end.x, end.y), Coordinate(end.x, start.y));
 
-    return newPath;
+    return astar.pathFinder(start, north, end);
 }
 
 
