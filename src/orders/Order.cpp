0cfbd378fed25e32ca07945c9f7134ff40e535f2
Groovesmith <jglad16@student.aau.dk>
2019-11-29 12:03:55 +0100

    add tests

--- src/orders/Order.cpp
+++
@@ -1,8 +0,0 @@
-#include "Order.h"
-
-Order::Order(int orderID, int timestampBorn, int PodID) : m_orderID(orderID),
-                                                          m_timestamp_born(timestampBorn),
-                                                          m_PodID(PodID),
-                                                          underWay(false){};
-
-Order::~Order() {}
