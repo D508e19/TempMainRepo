7c29de5d5553fecda4dbc0a74571cb421ca8d5bf
Groovesmith <jglad16@student.aau.dk>
2019-12-02 10:53:06 +0100

    tests made ready

--- tests/unit_tests/TestTemplate.cpp
+++ tests/unit_tests/TestTemplate.cpp
@@ -1,1 +1,15 @@
-#include "gtest/gtest.h"
+#include "src/includefile.h"
+
+#include <gtest/gtest.h>
+
+// this is incomplete
+TEST(MyTestSuit, MyTestCase)
+{
+    ASSERT_FALSE(true);
+}
+
+TEST(testname, testcase)
+{
+    Coordinate cord = Coordinate(2, 8);
+    ASSERT_EQ(cord.x, 2);
+}
