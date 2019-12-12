40002b5683dfafafa7c0e9badb14df03126c52b6
judo347 <mikkelhvk@hotmail.com>
2019-12-12 20:00:07 +0100

    Cleanup: PickStationManager.

--- src/imp/PickStationManager.cpp
+++ src/imp/PickStationManager.cpp
@@ -15,9 +15,31 @@ void PickStationManager::SetupPickStationManager(Warehouse* _wh, int numberOfPic
 
 void PickStationManager::Tick()
 {
-    for(auto n : stations)
+    for(auto stationPair : stations)
     {
-        n.second->Tick();
+        stationPair.second->Tick();
+    }
+}
+
+void PickStationManager::GeneratePickStations(int startzone_width, int arena_height, int numberOfPickingStations)
+{
+    if(startzone_width == 0)
+    {
+        argos::LOGERR << "PickStationManager: Starting zone with is 0! No picking stations was created!" << std::endl;
+        return;
+    }
+
+    if(arena_height < numberOfPickingStations)
+    {
+        argos::LOGERR << "PickStationManager: Arena height is less then the desired number of picking stations! No picking stations was created!" << std::endl;
+        return;
+    }
+        
+    int slot_height = arena_height / numberOfPickingStations;
+    for(int i = 0; i < numberOfPickingStations; i++)
+    {
+        int y = slot_height * i + (slot_height / 2);
+        AddPickingStation(i, std::pair<int,int>(0, y));
     }
 }
 
@@ -27,25 +49,10 @@ void PickStationManager::AddPickingStation(int id, std::pair<int,int> coordinate
     stations[id] = newStation;
 }
 
-void PickStationManager::GeneratePickStations(int startzone_width, int arena_height, int numberOfPickingStations)
-{
-    if(startzone_width == 0)
-        return;
-
-    if(arena_height <= numberOfPickingStations)
-        return;
-    
-    int slot_height = arena_height / numberOfPickingStations;
-    for(int i = 0; i < numberOfPickingStations; i++)
-    {
-        int y = slot_height * i + (slot_height / 2);
-        AddPickingStation(i, std::pair<int,int>(0, y));
-    }
-}
-
 PickStation* PickStationManager::GetPickStationForOrder()
 {   
     //TODO Could be replaced with logic to return the stations with the least amount of orders
+    // Currently it picks a station at random
     return stations[floor(rand() % stations.size())]; 
 }
 
