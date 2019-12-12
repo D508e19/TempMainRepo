3ff0573df258b5088ad3e40e3a809ad8ea0eba3e
judo347 <mikkelhvk@hotmail.com>
2019-12-12 18:35:42 +0100

    Rest of the merge ;)

--- src/imp/RobotWrapper.cpp
+++ src/imp/RobotWrapper.cpp
@@ -16,15 +16,12 @@ RobotWrapper::RobotWrapper(Basicbot *bot, Pathfinder *pf):m_bot(bot), pfp(pf), w
 void RobotWrapper::Tick()
 {
     if (m_bot->currentInstruction == idle)
-<<<<<<< HEAD
     {   
         //Update location of pod while carrying
         if(m_bot->isCarrying){
             currentOrder->podPtr->location = std::pair<int,int>(m_bot->lastReadCellQR.x, m_bot->lastReadCellQR.y);
         }
-=======
-    {
->>>>>>> dev
+
         if(instructionQueue.empty())
         {
             waitingForOrder = true;
@@ -44,9 +41,9 @@ void RobotWrapper::ProcessNewOrder()
 
     int TC = pfp->em->tickCounter; // TODO: set to LastTick. If LastTick is in the past set to current tick
 
-    Coordinate coordPod = Coordinate(currentOrder->podLocation.first, currentOrder->podLocation.second);
-    Coordinate coordPick = Coordinate(currentOrder->pickStationLocation.first, currentOrder->pickStationLocation.second);
-    Coordinate coordPodParkingSpot = Coordinate(currentOrder->podLocation.first, currentOrder->podLocation.second);
+    Coordinate coordPod = Coordinate(currentOrder->podPtr->location.first, currentOrder->podPtr->location.second);
+    Coordinate coordPick = Coordinate(currentOrder->pickStation->pickCoordinate.first, currentOrder->pickStation->pickCoordinate.second);
+    Coordinate coordPodParkingSpot = Coordinate(currentOrder->podPtr->location.first, currentOrder->podPtr->location.second);
 
     Path pathToPickingStation;
     Path pathToPod;
